3d214645c307b488283acf287c6e4659bfc7e34b
vanGalilea <z.galili@icloud.com>
2017-08-25 15:58:48 +0200

    websocket and config

---
+++ src/main/java/com/greglturnquist/payroll/WebSocketConfiguration.java
@@ -0,0 +1,30 @@
+package com.greglturnquist.payroll;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Component
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+    static final String MESSAGE_PREFIX = "/topic";
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/payroll").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker(MESSAGE_PREFIX);
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
+// end::code[]
